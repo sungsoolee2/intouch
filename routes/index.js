f0b7dd452127ed75706c09f4327e71928e77c847
Sung Lee <sungsoolee2@hotmail.com>
2019-07-16 14:02:22 -0500

    fixed index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
-app.get('/logout', (req, res) => {
-    if (req.userContext) {
-      const idToken = req.userContext.tokens.id_token;
-      const to = encodeURI(process.env.HOST_URL);
-      const params = `id_token_hint=${idToken}&post_logout_redirect_uri=${to}`;
-      req.logout();
-      res.redirect(
-        `${process.env.OKTA_ORG_URL}/oauth2/default/v1/logout?${params}`
-      );
-    } else {
-      res.redirect('/');
-    }
-  });
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+module.exports = router;
