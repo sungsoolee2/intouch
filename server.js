0c8b35e97f5b14f300e2960790658c87bb0b5489
Joey Lansangan <joeylansangan@gmail.com>
2019-07-16 13:45:07 -0500

    master changed

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
@@ -96,25 +95,3 @@ db.sequelize.sync(syncOptions).then(function() {
 });
 
 module.exports = app;
-=======
-// Dependencies
-var express = require("express");
-var exphbs = require("express-handlebars");
-
-// Create an instance of the express app.
-var app = express();
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 3500;
-
-// Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
->>>>>>> a9c38e21735ddd47ae5eb0abe5fe5717812cd417
