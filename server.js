4562d9d4d50aaf452aa117d67c549bd277b35e82
Joey Lansangan <joeylansangan@gmail.com>
2019-07-18 11:49:42 -0500

    fixed render css on handlebar files

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
+// app.use('/static', express.static('public'));
 
 // Okta auth middleware
 app.use(
@@ -88,14 +89,14 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+// db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
       PORT
     );
-  });
+  // });
 });
 
 module.exports = app;
